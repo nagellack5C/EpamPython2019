559816621babbb9c88661a8507782e86c1021248
Alexander Klimov <fatsumitsu@gmail.com>
2019-05-17 22:07:04 +0300

    solved

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -9,6 +9,12 @@
 
 def is_permutation(a: str, b: str) -> bool:
     # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
+    if len(a) - len(b):
+      return False
+    
+    for i in a:
+      b = b.replace(i, "", 1)
+    return not b
 
 
 assert is_permutation('baba', 'abab')
