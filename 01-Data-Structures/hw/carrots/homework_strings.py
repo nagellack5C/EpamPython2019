c4c070269f884cbc9a6d90138687f8f2ca064590
nagellack5C <fatsumitsu@gmail.com>
2019-05-23 16:40:06 +0300

    solved?

--- 01-Data-Structures/hw/carrots/homework_strings.py
+++ 01-Data-Structures/hw/carrots/homework_strings.py
@@ -34,25 +34,73 @@ P.S. За незакрытый файловый дескриптор - кара�
 """
 
 # read the file dna.fasta
-dna = None
+with open("files/dna.fasta") as dna_file:
+    dna_lines = [line.replace("\n", "") for line in dna_file.readlines()]
+    # making a dict of genes with a list of gene names
+    # will keep the input order when outputting
+    dna = {}
+    genes = []
+    for line in dna_lines:
+        if ">" in line:
+            key = line
+            genes.append(key)
+            dna[line] = ""
+        else:
+            dna[key] += line #assuming that every file is formatted like this
+
+with open("files/rna_codon_table.txt") as codon_table_file:
+    codon_table = {}
+    for line in codon_table_file.readlines():
+        #doing some weird things to parse the codons table
+        line = line.replace("\n", "").replace(" "*6, " "*3).split(" "*3)
+        for codon_trans in line:
+            seq, codon = codon_trans.strip().split(" ")
+            codon_table[seq] = codon
 
 
 def translate_from_dna_to_rna(dna):
     
-    """your code here"""
-    
-    return rna
+    return dna.replace("T", "U")
 
 
 def count_nucleotides(dna):
-    
-    """your code here"""
+
+    num_of_nucleotides = f'G - {dna.count("G")}, ' \
+                         f'A - {dna.count("A")}, ' \
+                         f'T - {dna.count("T")}, ' \
+                         f'C - {dna.count("C")}'
     
     return num_of_nucleotides
 
 
 def translate_rna_to_protein(rna):
     
-    """your code here"""
+    index = 3
+    protein = []
+    while index <= len(rna):
+        # pass over the rna
+        # will skip trailing symbols if they do not form a triplet
+        protein.append(codon_table[rna[index-3:index]])
+        index += 3
     
     return protein
+
+
+with open("nucl_stats.txt", "w") as nucl_stats,\
+        open("rnas.txt", "w") as rnas,\
+        open("proteins.txt", "w") as proteins:
+    for gene in genes:
+        nucl_stats.write(gene + "\n")
+        nucl_stats.write(count_nucleotides(dna[gene]) + "\n")
+
+        rnas.write(gene + "\n")
+        rna = translate_from_dna_to_rna(dna[gene])
+        rna_prettified = [rna[i:i + 75] for i in range(0, len(rna), 75)]
+        for line in rna_prettified:
+            rnas.write(line + "\n")
+
+        proteins.write(gene + "\n")
+        prot = translate_rna_to_protein(rna)
+        prot_prettified = [prot[i:i + 40] for i in range(0, len(prot), 40)]
+        for line in prot_prettified:
+            proteins.write(" ".join(line) + "\n")
