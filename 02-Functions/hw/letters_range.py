6b158816de9fa264931acaf648a88695bb68d867
nagellack5C <fatsumitsu@gmail.com>
2019-05-29 18:07:25 +0300

    solved?

--- 02-Functions/hw/letters_range.py
+++ 02-Functions/hw/letters_range.py
@@ -41,7 +41,9 @@ print(letters_range("b", "j", 2))
 print(letters_range("j", "b", **{'l': 7, 'o': 0}))
 print(letters_range("k", "z", **{'l': 7, 'o': 0}))
 
+
 def test(**kwargs):
     print(kwargs)
+
 
 print([i for i in range(10, -1)])
