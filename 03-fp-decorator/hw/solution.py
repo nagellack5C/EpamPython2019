60fdc442af0e2098067d6d1c5a5e63de45e663e0
nagellack5c <fatsuitsu@gmail.com>
2019-05-30 23:18:50 +0300

    resurrected

--- 03-fp-decorator/hw/solution.py
+++ 03-fp-decorator/hw/solution.py
@@ -1,4 +1,6 @@
 from functools import reduce
+from time import time
+from timeit import timeit
 
 # hw1
 
@@ -13,5 +15,11 @@ print(x)
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-mbyn = [[(m*m - n*n, 2*m*n, m*m + n*n) for m in range(1, 32) if (m*m - n*n)**2 + (2*m*n)**2 == (m*m + n*n)**2 and 2*m*m + 2*m*n == 1000] for n in range(1, 32)]
-print(mbyn)
+simple = [a * b * (1000 - a - b) for a in range(1, 1001) for b in range(1, 1001) if a*a + b*b == (1000 - a - b)**2][0]
+euclid = [a*b*c for a, b, c in [(m*m - n*n, 2*m*n, m*m + n*n) for m in range(1, 32) for n in range(1, 32)] if a * a + b * b == c * c and a + b + c == 1000][0]
+print(simple, euclid)
+
+# euclid is 500 times faster
+x = timeit("[a * b * (1000 - a - b) for a in range(1, 1001) for b in range(1, 1001) if a*a + b*b == (1000 - a - b)**2][0]", number=5)
+y = timeit("[a*b*c for a, b, c in [(m*m - n*n, 2*m*n, m*m + n*n) for m in range(1, 32) for n in range(1, 32)] if a*a+b*b == c*c and a + b + c == 1000][0]", number=5)
+print(x/y)
