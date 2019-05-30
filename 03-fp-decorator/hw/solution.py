5a76fde6366ac52e4067aea83dddab6a37adb1f4
nagellack5c <fatsuitsu@gmail.com>
2019-05-30 23:24:54 +0300

    formatting

--- 03-fp-decorator/hw/solution.py
+++ 03-fp-decorator/hw/solution.py
@@ -15,10 +15,10 @@ print(x)
 # Find the product abc.
 
 simple = [a * b * (1000 - a - b) for a in range(1, 1001) for b in range(1, 1001) if a*a + b*b == (1000 - a - b)**2][0]
-euclid = [a*b*c for a, b, c in [(m*m - n*n, 2*m*n, m*m + n*n) for m in range(1, 32) for n in range(1, 32)] if a * a + b * b == c * c and a + b + c == 1000][0]
+euclid = [a * b * c for a, b, c in [(m * m - n * n, 2 * m * n, m * m + n * n) for m in range(1, 32) for n in range(1, 32)] if a * a + b * b == c * c and a + b + c == 1000][0]
 print(simple, euclid)
 
-# euclid is 500 times faster
+# euclid is 500-800 times faster
 x = timeit("[a * b * (1000 - a - b) for a in range(1, 1001) for b in range(1, 1001) if a*a + b*b == (1000 - a - b)**2][0]", number=5)
-y = timeit("[a*b*c for a, b, c in [(m*m - n*n, 2*m*n, m*m + n*n) for m in range(1, 32) for n in range(1, 32)] if a*a+b*b == c*c and a + b + c == 1000][0]", number=5)
-print(x/y)
+y = timeit("[a * b * c for a, b, c in [(m * m - n * n, 2 * m * n, m * m + n * n) for m in range(1, 32) for n in range(1, 32)] if a * a + b * b == c * c and a + b + c == 1000][0]", number=5)
+print(x / y)
