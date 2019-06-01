4bfaec97a80e6cfc180811c74e2ce0ba4c6329d5
nagellack5c <fatsuitsu@gmail.com>
2019-06-01 17:51:11 +0300

    solved

--- 03-fp-decorator/hw/solution.py
+++ 03-fp-decorator/hw/solution.py
@@ -1,5 +1,7 @@
+import time
 from functools import reduce
 from timeit import timeit
+
 
 # hw1
 
@@ -48,16 +50,91 @@ print(last_ten)
 def is_armstrong(number):
     return number == sum(list(map(lambda x: int(x)**len(str(number)), list(str(number)))))
 
+
 assert is_armstrong(153) == True, 'Число Армстронга'
 assert is_armstrong(10) == False, 'Не число Армстронга'
 
 # hw3
 
+
 def collatz_steps(n):
     return "bad value!" if not isinstance(n, int) or n < 1 else n-1 if n == 1 else 1 + collatz_steps(n // 2) if n % 2 == 0 else 1 + collatz_steps(n * 3 + 1)
+
 
 print(collatz_steps(33))
 
 # assert collatz_steps(16) == 4
 # assert collatz_steps(12) == 9
 # assert collatz_steps(1000000) == 152
+
+# hw4
+# Переписать функцию make_cache, которая сохраняет
+# результаты предыдущих вызовов оборачиваемой функции,
+# таким образом, чтобы она сохраняла результаты в своем
+# хранилищe на определенное время, которое передается
+# параметром (аргументом) в декоратор.
+# Плюс придумать некоторый полезный юзкейс и заимплементировать функцию slow_function
+
+
+def make_cache(ttl):
+    cache = {}
+
+    def outer_wrapper(func):
+
+        def wrapper(*args, **kwargs):
+
+            # comparing current timestamp with cached results' TTL
+            # removing key-value pair if difference exceeds the
+            # ttl parameter
+            cur_time = time.time()
+            # using list(cache.keys()) to allow for removal while iterating
+            for i in list(cache.keys()):
+                if cur_time - cache[i]["ttl"] > ttl:
+                    cache.pop(i)
+
+            hashed_key = str(args) + str(kwargs)
+            if hashed_key not in cache:
+                cache[hashed_key] = {}
+                cache[hashed_key]["res"] = func(*args, **kwargs)
+                cache[hashed_key]["ttl"] = time.time()
+            return cache[hashed_key]["res"]
+        return wrapper
+
+    return outer_wrapper
+
+
+@make_cache(6)
+def slow_function(curr_name):
+    global curr_rates
+    '''
+    This function pulls currency rates from a dummy API
+    (in this case a global dict). time.sleep() simulates
+    connection time.
+    In this simulated case connection to the API requires 2
+    seconds and the results are stored for 5 seconds.
+    :param num:
+    :return:
+    '''
+    time.sleep(2)
+    return f'Exchange rate for {curr_name} is {curr_rates[curr_name]}'
+
+
+curr_rates = {
+    "USD": 30,
+    "RUR": 1,
+    "EUR": 40,
+    "GBP": 50,
+    "JPY": 0.5
+}
+
+
+# simulating user requests
+
+print(slow_function("USD"))
+curr_rates["USD"] = 60  # this value was increased in the source but the cached one is kept
+print(slow_function("USD"))
+print(slow_function("EUR"))
+print(slow_function("GBP"))
+print(slow_function("RUR"))
+print(slow_function("JPY"))
+print(slow_function("USD"))  # by this time the cached value should expire and the function will pull the new one
